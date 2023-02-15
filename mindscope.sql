4163fba8c474e4b841b456b35cb26f299d6fdacb
bevamanfu <111974995+bevamanfu@users.noreply.github.com>
2023-02-15 22:16:34 +0000

    Update mindscope.sql

--- mindscope.sql
+++ mindscope.sql
@@ -4,11 +4,11 @@ Create database MindScope;
 Use Mindscope;
 
 CREATE TABLE Web_user (
-	web_userID tinyint auto_increment,
-	userFname varchar(20) NOT NULL,
+    web_userID tinyint auto_increment,
+    userFname varchar(20) NOT NULL,
     userLname varchar(27) NOT NULL,
     Inst_ID tinyint UNIQUE,
-	DOB  date,
+    DOB  date,
     phone_number varchar(14) NOT NULL UNIQUE,
     email_address varchar(60) NOT NULL UNIQUE,
     department varchar(50) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE Web_user (
 
 
 CREATE TABLE Student(
-	web_userID tinyint,
+    web_userID tinyint,
     year_group int NOT NULL,
     major varchar(25),
     
@@ -28,7 +28,7 @@ CREATE TABLE Student(
 
 
 CREATE TABLE CP_Staff (
-	 web_userID tinyint,
+     web_userID tinyint,
      user_role varchar(20) NOT NULL,
      title varchar (20) NOT NULL,
      dateJoined date,
@@ -38,7 +38,7 @@ CREATE TABLE CP_Staff (
 
 
 CREATE TABLE Therapy_session(
-	therapyID int auto_increment,
+    therapyID int auto_increment,
     instructor_name varchar(47),
     duration time NOT NULL,
     location varchar(20),
@@ -48,8 +48,8 @@ CREATE TABLE Therapy_session(
 
 
 CREATE TABLE Individual_Sess(
-	therapyID int,
-    category varchar(20),
+     therapyID int,
+     category varchar(20),
      PRIMARY KEY(therapyID)
 );
 
@@ -62,21 +62,24 @@ CREATE TABLE Group_Session (
 
 
 CREATE TABLE Booking (
-	bookingID int auto_increment,
-	appointment_date date NOT NULL, 
+    bookingID int auto_increment,
+    appointment_date date NOT NULL, 
     start_time time NOT NULL,
     StudentID tinyint NOT NULL, -- Student ID --
     InstructorID tinyint NOT NULL, -- Instructor ID --
+    therapyID int,
     
     PRIMARY KEY(bookingID),
     FOREIGN KEY(StudentID) REFERENCES Web_user(web_userID) ON DELETE CASCADE,
-	FOREIGN KEY(InstructorID) REFERENCES Web_user(web_userID) ON DELETE CASCADE
+    FOREIGN KEY(InstructorID) REFERENCES Web_user(web_userID) ON DELETE CASCADE,
+    FOREIGN KEY(therapyID) REFERENCES Therapy_session(therapyID)ON DELETE CASCADE
+
 
 );
 
 
 CREATE TABLE Review(
-	reviewID tinyint auto_increment,
+    reviewID tinyint auto_increment,
     event_name varchar(15) NOT NULL,
     review_date date,
     user_name varchar(47) NOT NULL,
@@ -90,7 +93,7 @@ CREATE TABLE Review(
 
 
 CREATE TABLE Messages(
-	messageID int auto_increment,
+    messageID int auto_increment,
     sender_name varchar(50) NOT NULL,
     message_content text NOT NULL,
     receiver_name varchar(50) NOT NULL,
@@ -100,7 +103,7 @@ CREATE TABLE Messages(
 );
 
 CREATE TABLE  MessageUser(
-	web_userID tinyint,
+    web_userID tinyint,
     messageID int,
     
     FOREIGN KEY(web_userID) REFERENCES Web_User(web_userID) ON DELETE CASCADE,
