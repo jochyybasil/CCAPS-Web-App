9e25723adeaefea6a90590528798861a7e874596
jt-manful <johnmanful2002@hotmail.com>
2023-02-16 21:36:31 +0000

    new

--- session.php
+++ session.php
@@ -3,7 +3,7 @@
     session_start();
     $user_check = $_SESSION['login_user'];
 
-    $ses_sql = mysqli_query($conn,"select web_userID, email_address from web_user where email ='$user_check'");
+    $ses_sql = mysqli_query($conn,"select web_userID, email_address from Web_user where email ='$user_check'");
     
     $row = mysqli_fetch_assoc($ses_sql,MYSQLI_ASSOC);
     $loggedin_session = $row['email_address'];
