3482456ed50356eab61b3289dd6098a057659b1d
jt-manful <johnmanful2002@hotmail.com>
2023-02-20 22:45:40 +0000

    new

--- MODEL/db.php
+++ MODEL/db.php
@@ -8,7 +8,7 @@ $dbname = "";
 $conn = mysqli($servername,$username,$password,$dbname);
 
 if ($conn->connect_error){
-    die("Connection failed", $$conn->connect_error);
+    die("Connection failed", $conn->connect_error);
 }
 
 ?>
