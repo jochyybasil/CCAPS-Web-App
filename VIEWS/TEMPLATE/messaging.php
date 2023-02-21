e1e8e8b2c08bb099b344beb0a106020946f2e1d3
jt-manful <johnmanful2002@hotmail.com>
2023-02-21 22:25:11 +0000

    new

--- VIEWS/TEMPLATE/messaging.php
+++ VIEWS/TEMPLATE/messaging.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rant Box</title>
-    <link rel="stylesheet" href="CSS/stylesheet.css <?php echo time(); ?>">
+    <link rel="stylesheet" href="CSS/stylesheet.css" <?php echo time(); ?>>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
@@ -69,7 +69,7 @@
                     </div>
                 
                     <div class="container1 darker">
-                    <img src="john_manful.jpg" alt="Avatar" class="right" style="width:100%;">
+                    <img src="IMAGES/john_manful.jpg" alt="Avatar" class="right" style="width:100%;">
                     <p style="text-align: right;">Hey! I'm fine. Thanks for asking!</p>
                     <span class="time-left">11:01</span>
                     </div>
@@ -81,7 +81,7 @@
                     </div> -->
                 
                     <div class="container1 darker">
-                    <img src="john_manful.jpg" alt="Avatar" class="right" style="width:100%;">
+                    <img src="IMAGES/john_manful.jpg" alt="Avatar" class="right" style="width:100%;">
                     <p style="text-align: right;">I'm honestly tired of school. When does it ever end? I want out. Ughhh!!!!</p>
                     <span class="time-left">11:05</span>
                     </div>
