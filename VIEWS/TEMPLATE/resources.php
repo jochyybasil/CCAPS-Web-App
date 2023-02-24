982d6022da6253ed205c86391b199168fb2dfc02
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-02-24 09:26:03 +0000

    resources page

--- VIEWS/TEMPLATE/resources.php
+++ VIEWS/TEMPLATE/resources.php
@@ -8,6 +8,34 @@
     <title>Resources</title>
     <link rel="stylesheet" href="CSS/stylesheet.css" <?php echo time(); ?>>
     <link rel="stylesheet" href="CSS/res_style.css" <?php echo time(); ?>>
+
+
+    <style>
+		.container {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-evenly;
+			align-items: center;
+			margin: 20px;
+		}
+		.square {
+			width: 200px;
+			height: 200px;
+			margin: 10px;
+			box-shadow: 2px 2px 5px #888888;
+			position: relative;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+		}
+		
+		.caption {
+			text-align: center;
+			margin-top: 10px;
+			font-size: 18px;
+			color: #888888;
+		}
+	</style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
@@ -61,63 +89,47 @@
 <body>
 
 
-    <div class="container" style="font-weight:bolder;">
-        <div class="row">
-            <div class="col">
-            <div class="circle_case">
-        
-        <div  id="div1">
-            <span class="circ"> Peer Counselors </span>
-            <br>
-            <br>
-            <br>
-            <br>
-            <span class="circ"> Yoga & Breathing Videos </span>
-           
-        </div>
-
-        <div id="div2">
-            <span class="circ"> Peer Tutors </span>
-            <br>
-            <br>
-            <br>
-            <br>
-            <span class="circ"> Talent Hunt </span>
-           
-        </div>
-
-        <div id="div3">
-            <span class="circ"> Mental Health </span>
-            <br>
-            <br>
-            <br>
-            <br>
-            <span class="circ"> Club Registration Form </span>
-            
-        </div>
-    </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+		<div class="square">
+			<a href="ashesi.edu.gh">Peer Counsellors</a>
+			<!-- <div class="caption">Caption A</div> -->
+		</div>
+		<div class="square">
+			<a href="https://www.ashesi.edu.gh">Peer Tutors</a>
+			<!-- <div class="caption">Caption B</div> -->
+		</div>
+		<div class="square">
+			<a href="https://www.ashesi.edu.gh">Mental Health Awareness</a>
+			<!-- <div class="caption">Caption C</div> -->
+		</div>
+		<div class="square">
+			<a href="https://www.ashesi.edu.gh">Academic Advisor</a>
+			<!-- <div class="caption">Caption D</div> -->
+		</div>
+		<div class="square">
+			<a href="messaging.php">Rant Box</a>
+			<!-- <div class="caption">Caption E</div> -->
+		</div>
+	</div>
 
     
 
 
 
   <!-- footer -->
-  <footer class="bg-dark py-3">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <p>&copy; 2023 MindScope Live. All Rights Reserved.</p>
+  <footer class="bg-dark py-3" style="margin-top:120px;">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <p>&copy; 2023 MindScope Live. All Rights Reserved.</p>
+            </div>
+            <div class="col-md-6">
+            <ul class="list-unstyled float-md-end">
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+            </div>
         </div>
-        <div class="col-md-6">
-          <ul class="list-unstyled float-md-end">
-            <li><a href="contact.php">Contact Us</a></li>
-          </ul>
         </div>
-      </div>
-    </div>
 </footer>
 </body>
 </html>
