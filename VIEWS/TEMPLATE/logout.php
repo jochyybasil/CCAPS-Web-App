b21222e6c72d706891a382feff8fd54f8844c9e6
jt-manful <johnmanful2002@hotmail.com>
2023-03-02 22:51:41 +0000

    new

--- VIEWS/TEMPLATE/logout.php
+++ VIEWS/TEMPLATE/logout.php
@@ -1,10 +1,10 @@
 <?php
  include "../MODEL\db.php";
  include  "../MODEL\session.php";
- 
+
 session_start();
 //destroy the session
 session_unset();
 //redirect to login page
-header("location: login.php");
+header("location: login.php?logout=true");
 ?>
