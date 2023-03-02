0e2bb55994292e54020a540db51ff8e1ac7b4e74
jt-manful <johnmanful2002@hotmail.com>
2023-03-02 21:51:37 +0000

    NEWEST COMMIT

--- VIEWS/TEMPLATE/student_profile.php
+++ VIEWS/TEMPLATE/student_profile.php
@@ -10,8 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile</title>
-    <link rel="stylesheet" href="stylesheet.css">
-    <link rel="stylesheet" href="home_page/stylesheet.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="CSS/stylesheet.css?<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/profile.css? <?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -59,18 +58,25 @@
     </nav>
 </header>
 <body>
-    <div>
+
  
-    <?php
-      $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or die('query failed');
+    <!-- <? php
+      $select = mysqli_query($conn, "SELECT * FROM `user_profile` WHERE web_userID = '$loggedin_id'");
       if(mysqli_num_rows($select) > 0){
          $fetch = mysqli_fetch_assoc($select);
       }
-   ?>
+   ?> -->
 
     <form method="post">  
     <div class="profile-pic-container">
         <div class="profile-pic">
+        <!-- <?php
+         if($fetch['user_image'] == ''){
+            // echo '<i class="fa-thin fa-user-tie"></i>';
+         }else{
+            echo '<img src="uploaded_img/'.$fetch['user_image'].'">';
+         }
+         ?> -->
         <style>
          /* echo '<img src="uploaded_img/'.$fetch['image'].'">'; */
          
@@ -80,7 +86,7 @@
         <div class="info-content">
         <br>
         <span>update your pic :</span>
-            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
+            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" >
         </div>
     </div>
     </form>
@@ -113,7 +119,7 @@
     </form>
 
 
-    </div>
+
 </body>
 
 </html>
