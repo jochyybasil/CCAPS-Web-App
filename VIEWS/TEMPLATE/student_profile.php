1a17e967157592df2dbb6c47bd32337847a3399d
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-03-02 21:21:46 +0000

    student profile header

--- VIEWS/TEMPLATE/student_profile.php
+++ VIEWS/TEMPLATE/student_profile.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="CSS/stylesheet.css? <?php echo time(); ?>">
     <link rel="stylesheet" href="home_page/stylesheet.css?<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/profile.css? <?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -67,8 +67,8 @@
         </div>
         <div class="info-content">
         <br>
-        <span>update your pic :</span>
-            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png" class="box">
+        <span>Update your photo :</span>
+            <input type="file" name="update_image" accept="image/jpg, image/jpeg, image/png">
         </div>
     </div>
     </form>
@@ -95,7 +95,7 @@
             <input name="user_major" value="<?php echo $row['department']; ?>" type="text">
             
             <br>
-            <input type="submit" class="edit-profile-text" value ="Edit Profle" ></p>
+            <input type="submit" class="edit-profile-text" value ="Edit Profile" ></p>
 
 
         </div>
