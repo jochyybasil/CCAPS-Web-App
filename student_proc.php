a0da77d8cfc9cbb1ecd05e23aa5a6ee2f18bbeb6
jt-manful <johnmanful2002@hotmail.com>
2023-03-03 02:08:08 +0000

    we

--- student_proc.php
+++ student_proc.php
@@ -2,40 +2,45 @@
     include 'db.php';
     include "session.php";
 
-//$loggedin_session
-    if(isset($_POST['Update Profile'])){
+    if(isset($_POST["update_profile"])){
 
-    $fname =  $_POST['user_fname'];
-    $lname =  $_POST['user_lname'];
-    $gender =  $_POST['user_gender'];
-    $phoneN =  $_POST['user_phone'];
-    $major =  $_POST['user_major'];
+        echo "works";
 
-    $sql = "UPDATE user_profile SET userFname='$fname', userLname='$lname', gender='$gender', phone_number='$phoneN', department='$major' WHERE web_userID = $loggedin_userID";
+    // $fname =  $_POST['user_fname'];
+    // $lname =  $_POST['user_lname'];
+    // $gender =  $_POST['user_gender'];
+    // $phoneN =  $_POST['user_phone'];
+    // $major =  $_POST['user_major'];
 
-    if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-    } else {
-    echo "Error updating record: " . $conn->error;
-    }
+    // echo "$fname";
+    // $sql = "UPDATE user_profile SET userFname='$fname', userLname='$lname', gender='$gender', phone_number='$phoneN', department='$major' WHERE web_userID = $loggedin_userID";
+    
+    // if ($conn->query($sql) === TRUE) {
+    // echo "Record updated successfully";
+    // } 
+    // else {
+    // echo "Error updating record: " . $conn->error;
+    // }
 
-    $update_image = $_FILES['update_image']['name'];
-    $update_image_size = $_FILES['update_image']['size'];
-    $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
-    $update_image_folder = 'uploaded_img/'.$update_image;
+    // $update_image = $_FILES['update_image']['name'];
+    // $update_image_size = $_FILES['update_image']['size'];
+    // $update_image_tmp_name = $_FILES['update_image']['tmp_name'];
+    // $update_image_folder = 'uploaded_img/'.$update_image;
 
-    if(!empty($update_image)){
-        if($update_image_size > 2000000){
-            $message[] = 'image is too large';
-        }else{
-            $image_update_query = mysqli_query($conn, "UPDATE `user_profile` SET user_image = '$update_image' WHERE web_userID = $loggedin_userID") or die('query failed');
-            if($image_update_query){
-                move_uploaded_file($update_image_tmp_name, $update_image_folder);
-            }
-            $message[] = 'image updated succssfully!';
-        }
-    }
+    // if(!empty($update_image)){
+    //     if($update_image_size > 2000000){
+    //         $message[] = 'image is too large';
+    //     }else{
+    //         $image_update_query = mysqli_query($conn, "UPDATE `user_profile` SET user_image = '$update_image' WHERE web_userID = $loggedin_userID") or die('query failed');
+    //         if($image_update_query){
+    //             move_uploaded_file($update_image_tmp_name, $update_image_folder);
+    //         }
+    //         $message[] = 'image updated succssfully!';
+    //     }
+    // }
 
+    }else{
+        echo"no update";
     }
 
 ?>
