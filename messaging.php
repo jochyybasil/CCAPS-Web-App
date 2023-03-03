dbdf58c7d904999e45449b1fbf25dfc7ca753203
Aaron Tsatsu Tamakloe <aarontmkl@gmail.com>
2023-03-03 01:38:38 +0000

    message action

--- messaging.php
+++ messaging.php
@@ -94,7 +94,7 @@
                                         <label for="floatingTextarea">Send a message...</label>
                                       </div>
                                       <br>
-                                      <button type="submit" class="btn btn-primary" style="margin-top: 5px;">Send</button>
+                                      <button type="submit" class="btn btn-primary" style="margin-top: 5px;" action="message_proc.php">Send</button>
                                     </div>
                                 </div>
                             </div>
